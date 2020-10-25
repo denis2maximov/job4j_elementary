6f6a577b72acee4ce6729e9ebb1abb2840c8dc2d
games-pc <denis2maksimov@gmail.com>
2020-10-25 21:16:10 +0300

    9.1. String [#394812] поправил замечание CheckStyle -  и 6 строка

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl1 = x2-x1;
-        double rsl2 = y2-y1;
+        double rsl1 = x2 - x1;
+        double rsl2 = y2 - y1;
         double first1 = Math.pow(rsl1, 2);
         double first2 = Math.pow(rsl2, 2);
         double sum = first1 + first2;
