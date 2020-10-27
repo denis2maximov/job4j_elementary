59c68c196d32861069daf364baa9fe80dd15eaae
games-pc <denis2maksimov@gmail.com>
2020-10-27 21:54:04 +0300

    1.1. Входные данные в тесте [#394840] x2

--- test/ru/job4j/calculate/X2Test.java
+++ test/ru/job4j/calculate/X2Test.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class X2Test {
 
     @Test
-    public void whenA10B9C0X2Then40() {
+    public void whenA10B0C0X2Then40() {
         /* Входные параметры. Их будет 4 штуки */
         int a = 10;
         int b = 0;
@@ -21,4 +21,38 @@ public class X2Test {
         /* сравнение полученного значения с ожидаемым */
         Assert.assertEquals(expected, rsl);
     }
+
+    @Test
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
 }
