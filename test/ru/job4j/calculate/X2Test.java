f3735bf48bddafcb39e6989931758db7238a064a
games-pc <denis2maksimov@gmail.com>
2020-10-27 21:59:24 +0300

    1.1. Входные данные в тесте [#394840] X2Test.java

--- test/ru/job4j/calculate/X2Test.java
+++ test/ru/job4j/calculate/X2Test.java
@@ -34,6 +34,17 @@ public class X2Test {
     }
 
     @Test
+    public void whenA0B1C1X1Then3() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
     public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
