d80e5ab5285339ccd567bdbac86b9e3bba45c16e
dss <denis2maksimov@gmail.com>
2020-11-02 17:02:28 +0300

    2. Тесты для задачи "идеальный вес" [#394836]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -5,7 +5,13 @@ import org.junit.Test;
 
 public class FitTest {
 
-
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 
     @Test
     public void whenWoman170Then69() {
