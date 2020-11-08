96511be9693e221a5ecf875b75dd014ac74ab644
games-pc <denis2maksimov@gmail.com>
2020-11-08 18:02:43 +0300

    4.2. Тернарное сравнение [#394762] переделал с другими входнвми данными 2 последних теста

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl;
+        rsl = Math.abs(x2 - x1);
+        rsl = (Math.abs(x1 - x2) == Math.abs(y1 - y2)) ? rsl : 0;
+        return rsl;
+    }
+}
