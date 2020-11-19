a98b9a1e0e8eef2948fdb1d19beb3f8740716684
dss <denis2maksimov@gmail.com>
2020-11-19 16:10:08 +0300

    6.2.2. Переставить элементы массива [#394788]

---
+++ src/ru/job4j/loop/MultiplicationLoop.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class MultiplicationLoop {
+    public static int mult(int a, int b) {
+        int i = 0;
+        int rsl = 0;
+        for (i = a; i < b; i++) {
+           //* int rsl;
+          if (i == (int)i) {
+              rsl = i * i;
+            }
+        }
+        i = rsl;
+        return i; }
+         }
+
