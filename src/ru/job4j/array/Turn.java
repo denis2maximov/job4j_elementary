ba30ff055f81354453d6c22f381f123a711b2b10
dss <denis2maksimov@gmail.com>
2020-11-20 13:56:43 +0300

    6.2.3. Перевернуть массив. [#394786]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -1,15 +1,18 @@
 package ru.job4j.array;
 
 public class Turn {
-    //* проверяем кратно ли длинна массива 2,
-    // так мы узнаем четный или не четный массив.
-    // Потом если четный  - меняем местами все элементы деленные на 2 *//
     public static int[] back(int[] array) {
+
         for (int i = 0; i < array.length / 2; i++) {
+            for (i = 0; i < array.length / 2 - 1; i++) {
             int tmp = array[i];
-            array[i] = array[array.length - i -1];
-            array[array.length - i -1] = tmp;
-             }
-        return array;
-    }
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+            int tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+
+            return array; }
 }
