c70d2f68eabd1af9cdc61da7b42253431ad117c6
games-pc <denis2maksimov@gmail.com>
2020-11-21 00:17:08 +0300

    6.1. Классический поиск перебором. [#394768]  плюс 6.5. Поиск индекса в диапазоне. [#394776]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
 
-    @Test
+     @Test
     public void whenArrayHas5Then0() {
-        int input[] = {5, 4, 3, 2};
+        int[] input = {5, 4, 3, 2};
         int value = 5;
         int result = FindLoop.indexOf(input, value);
         int expect = 0;
@@ -17,10 +17,21 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas6ThenMinus1() {
-        int input[] = {5, 4, 3, 2};
+        int[] input = {5, 4, 3, 2};
         int value = 6;
         int result = FindLoop.indexOf(input, value);
         int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOfTwo(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
