c27248dd413044636720eca88a2e1c3f4cb3ac03
games-pc <denis2maksimov@gmail.com>
2020-11-21 13:21:57 +0300

    6.3. Массив заполнен true или false [#394772] поправил проверку на data[i] != data[0]

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -5,7 +5,7 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int i = 0; i < data.length; i++) {
-         if (data[i] != result) {
+         if (data[i] != data[0]) {
              return false;
          }
      }
