10992c59c76af51fe6317f3ef5e8c6800805bbd7
games-pc <denis2maksimov@gmail.com>
2020-11-21 16:47:02 +0300

    6.0.3.1. Массивы и цикл for-earch [#394793] - добавил вывод через цикл for-each

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class ArrayLoop {
+
     public static void main(String[] args) {
         int[] name = new int[5];
         for (int index = 0; index < name.length; index++) {
@@ -9,9 +10,8 @@ public class ArrayLoop {
       /*  for (int index = 0; index < name.length; index++) {
             System.out.println(name[index]);
         } */
-        for (int names : name ) {
+        for (int names : name) {
             System.out.println(names);
-
-        }
+      }
     }
 }
