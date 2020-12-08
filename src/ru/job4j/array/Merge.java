5fa7b32910ba681bf296b5830e91da96daeadd0f
dss <denis2maksimov@gmail.com>
2020-12-08 13:34:32 +0300

    Решение задачи для экзамена. Версия с for и rsl.length

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,8 +3,33 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int i = 0, j = 0, y = 0;
-        while (i < left.length && j < right.length) {
+        int iLeft = 0, jRight = 0;
+
+        for (int k = 0; k < rsl.length; k++) {
+
+            if (iLeft > left.length - 1) {
+                int a = right[jRight];
+                rsl[k] = a;
+                jRight++;
+            } else if (jRight > right.length - 1) {
+                int a = left[iLeft];
+                rsl[k] = a;
+                iLeft++;
+            } else if (left[iLeft] < right[jRight]){
+                int a = left[iLeft];
+                rsl[k] = a;
+                iLeft++;
+            }
+            else{
+                int b = right[jRight];
+                rsl[k] = b;
+                jRight++;
+            }
+        }
+        return rsl;
+    }
+}
+      /*  while (i < left.length && j < right.length) {
             rsl[y++] = left[i] < right[j] ? left[i++] : right[j++];
         }
         while (i < left.length) {
@@ -12,8 +37,6 @@ public class Merge {
         }
         while (j < right.length) {
             rsl[y++] = right[j++];
-        }
+        } */
 
-        return rsl;
-    }
-}
+
