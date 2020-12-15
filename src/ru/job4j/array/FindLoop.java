34ef8f6719303a9bc6fa1ef44e1a379e03242d4b
games-pc <denis2maksimov@gmail.com>
2020-12-15 19:47:40 +0300

    файлы стертые

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
@@ -13,8 +13,8 @@ public class FindLoop {
     }
 
     public static int indexOfTwo(int[] data, int el, int start, int finish) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
-        for (int index = 2; index < data.length - 1; index++) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
             if (data[index] == el) {
                 rst = index;
                 return rst;
