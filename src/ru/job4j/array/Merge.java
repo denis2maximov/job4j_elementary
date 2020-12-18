b868d2f3516e90f181bc668c5c9c7fad09a01c00
dss <denis2maksimov@gmail.com>
2020-12-18 12:30:09 +0300

    перенес фигурные скобки на отдельную строку. Убрал пробелы в методе

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -4,9 +4,7 @@ public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int iLeft = 0, jRight = 0;
-
         for (int k = 0; k < rsl.length; k++) {
-
             if (iLeft > left.length - 1) {
                 int a = right[jRight];
                 rsl[k] = a;
@@ -29,14 +27,5 @@ public class Merge {
         return rsl;
     }
 }
-      /*  while (i < left.length && j < right.length) {
-            rsl[y++] = left[i] < right[j] ? left[i++] : right[j++];
-        }
-        while (i < left.length) {
-            rsl[y++] = left[i++];
-        }
-        while (j < right.length) {
-            rsl[y++] = right[j++];
-        } */
 
 
