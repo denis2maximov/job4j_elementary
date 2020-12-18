295c200929218579971411b7d9df41880f08c376
dss <denis2maksimov@gmail.com>
2020-12-18 13:21:54 +0300

    убрал пустые строки в методе

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -4,8 +4,7 @@ public class SqArea {
     public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double s = l * h;
-        return s;
+        return l * h;
     }
 
     public static void main(String[] args) {
