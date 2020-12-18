2b8a92a20d4f7db3ec864867954d12f5cd4b752b
dss <denis2maksimov@gmail.com>
2020-12-18 12:49:26 +0300

    убрал коментарии не оформленые как JavaDoc, поравил замечания CheckStyle

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -4,16 +4,15 @@ public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int index = 0; index < bound; index++) {
-            rst[index] = index * index;   /* заполнить массив через цикл элементами от 0 до bound , возведенными в квадрат */
-
+            rst[index] = index * index;
     }
     return rst;
     }
 
     public static void main(String[] args) {
         int[] array = calculate(4);
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+        for (int i : array) {
+            System.out.println(i);
         }
     }
 }
