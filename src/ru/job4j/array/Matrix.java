03f30c551ab3ac0172d2fdf1e5566ff4c35b54cd
dss <denis2maksimov@gmail.com>
2020-12-18 12:27:54 +0300

    перенес фигурные скобки на отдельную строку

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
-        int rsl = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 table[i][j]  = ((i + 1) * (j + 1));
                 }
         }
-        return table; }
+        return table;
+    }
 }
