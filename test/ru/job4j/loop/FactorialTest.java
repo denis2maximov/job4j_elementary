0bc94d02d37e027e180e50e385885dcf0be40835
dss <denis2maksimov@gmail.com>
2020-12-18 13:53:05 +0300

    убрал убрал комментарии не JavaDoc

--- test/ru/job4j/loop/FactorialTest.java
+++ test/ru/job4j/loop/FactorialTest.java
@@ -5,7 +5,6 @@ package ru.job4j.loop;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FactorialTest {
@@ -18,8 +17,6 @@ public class FactorialTest {
 
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        /* напишите здесь тест, проверяющий, что факториал для числа 0 равен 1. */
-        /* public void whenSumEvenNumbersFrom3ToTenThen8() { */
             int rsl = Factorial.calc(0);
             int expected = 1;
             assertThat(rsl, Matchers.is(expected));
