13d71050919c2bc76b69e8ff932b3882bddcb889
denis2maksimov <denis2maksimov@gmail.com>
2021-02-23 13:40:29 +0300

    Статичные и не статичные методв и классы. Вызов из статичного метода нестатичный метод

---
+++ src/ru/job4j/Mechanism.java
@@ -0,0 +1,51 @@
+package ru.job4j;
+
+public class Mechanism {
+    public static void main(String[] args) {
+        Auto auto = new Auto();
+        auto.autoDrive();
+        Lada lada = new Lada();
+        lada.autoDrive();
+      //  Auto.autoDrive(); // not work
+      //   Giguli.getLada(); // not work
+        Giguli g1 = new Giguli();
+        g1.getLada();
+            
+        class NotStatic{
+            public void nonStaticMetod(){
+                new Auto().autoDrive();
+                System.out.println("print notStaticMetod");
+            }
+        }
+    }
+
+    }
+
+
+    class Auto {
+        public void autoDrive() {
+            System.out.println("I am automobile");
+        }
+    }
+
+    class Lada extends Auto{
+        @Override
+        public void autoDrive() {
+            super.autoDrive();
+            System.out.println("I love Lada 21083");
+        }
+    }
+      class Giguli{
+        public void getLada() {
+           // Auto.autoDrive();
+            Lada lada = new Lada();
+            lada.autoDrive();
+
+         System.out.println("Giguli its Victory");
+            new Auto().autoDrive();
+
+        }
+    }
+
+
+
