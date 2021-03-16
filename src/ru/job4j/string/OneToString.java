73234e2e1006c4c064ecde80290de1e4e44edcac
dss <denis2maksimov@gmail.com>
2021-03-16 16:15:48 +0300

    OneToString

---
+++ src/ru/job4j/string/OneToString.java
@@ -0,0 +1,9 @@
+package ru.job4j.string;
+
+public class OneToString {
+    public static void main(String[] args) {
+        String string = new String("Новое слово");
+        System.out.println(string.charAt(2));
+        String stringTwo = new String("Программирование это здорово");
+        }
+    }
--- src/ru/job4j/string/StringOne.java
+++ src/ru/job4j/string/StringOne.java
@@ -10,11 +10,24 @@ public class StringOne {
         String b = new String( chars1,  2, 1);
         System.out.println(b);
         char c[] = {'J', 'a', 'v', 'a'};
-        System.out.println(c);
+        System.out.println("Длинна строки " + c.length);
         String c1 = new String(c);
         System.out.println(c);
         String c2 = new String(c1);
         System.out.println(c2);
+        byte ascii[] = {21,15,16,6,48};
+        String allo = new String(ascii);
+        System.out.println(allo);
+        String allo2 = new String (ascii, 2,3);
+        System.out.println(allo2);
+        String string = new String("abc");
+        System.out.println("abc".length());
+        String age = new String("9");
+        int o = 9;
+        String let = new String( " Лет");
+        String floy = new String("Сколько лет " + o + " сколько зим");
+        System.out.println("Ему " + age + let);
+        System.out.println(floy);
+
     }
 }
-//
